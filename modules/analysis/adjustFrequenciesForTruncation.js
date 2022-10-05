1ee9e861eadad315be0e658e5a09f3147d7a9b5f
Rob Gifford <robjgiff@gmail.com>
2022-10-05 09:34:47 +0100

    Refactor

--- modules/analysis/adjustFrequenciesForTruncation.js
+++
@@ -1,9 +0,0 @@
-// get frequencies for every alignment columns
-
-// get stop position in each sequence
-
-  // if stop occurs before end of constraining reference, adjust counts 
-  
-    // subtract 1 from all counts downstream if stop
-    
-    
