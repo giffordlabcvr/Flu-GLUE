a53e762daf6123ca6f3c34d18eed19b7df0184f2
Rob Gifford <robjgiff@gmail.com>
2022-10-10 06:20:07 +0100

    Misc updates

--- glue/build/genus/icv/icvLoadIsolateData.js
+++ glue/build/genus/icv/icvLoadIsolateData.js
@@ -1,7 +1,9 @@
 // list the sequences in source ncbi-icv-nuccore
 var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'ncbi-icv-nuccore'"]);
+
 // extract from the result a list of sequence IDs.
 var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
+
 // for each sequence ID
 _.each(seqIds, function(seqId) {
     // create an object in the custom table which uses the sequence ID as the row ID.
@@ -11,7 +13,7 @@ _.each(seqIds, function(seqId) {
         glue.command(["set", "link-target", "isolate_data", "custom-table-row/isolate_data/"+seqId]);
     });
 
-    // associate the corresponding sequence with this object.
+    // update sequence table fields for this isolate
     glue.inMode("sequence/ncbi-icv-nuccore/"+seqId, function() {
         glue.command(["set", "field", "genus", "Gammainfluenzavirus"]);
         glue.command(["set", "field", "name", "ICV"]);
