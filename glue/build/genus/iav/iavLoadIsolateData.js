0428928810f481e840dfbfc8add53263735a45b1
Rob Gifford <robjgiff@gmail.com>
2022-11-07 11:19:00 +0000

    Added IAV reference sequences and import logic.

--- glue/build/genus/iav/iavLoadIsolateData.js
+++ glue/build/genus/iav/iavLoadIsolateData.js
@@ -1,13 +1,28 @@
-// list the sequences in source ncbi-nuccore-iav
-var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'ncbi-nuccore-iav'"]);
-// extract from the result a list of sequence IDs.
-var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
-// for each sequence ID
-_.each(seqIds, function(seqId) {
-    // create an object in the custom table which uses the sequence ID as the row ID.
-    glue.command(["create", "custom-table-row", "isolate_data", seqId]);
-    // associate the corresponding sequence with this object.
-    glue.inMode("sequence/ncbi-nuccore-iav/"+seqId, function() {
-        glue.command(["set", "link-target", "isolate_data", "custom-table-row/isolate_data/"+seqId]);
-    });
+var segments = [ 1, 2, 3, 4, 5, 6, 7, 8 ];
+
+_.each(segments, function(segment) {
+
+    var sourceName = 'iav-ncbi-refseqs-seg' . segment;
+    
+	// list the sequences in source
+	var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = '"+sourceName+"'"]);
+
+	// extract from the result a list of sequence IDs.
+	var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
+
+	// for each sequence ID
+	_.each(seqIds, function(seqId) {
+
+		// create an object in the custom table which uses the sequence ID as the row ID.
+		glue.command(["create", "custom-table-row", "isolate_data", seqId]);
+	
+		// associate the corresponding sequence with this object.
+		glue.inMode("sequence/"+sourceName+"/"+seqId, function() {
+		
+			glue.command(["set", "link-target", "isolate_data", "custom-table-row/isolate_data/"+seqId]);
+			
+		});
+	
+	});
+
 });
