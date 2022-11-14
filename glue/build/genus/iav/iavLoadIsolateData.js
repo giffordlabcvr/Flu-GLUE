7926a889f91e357d5faf83d146fc5a83e3946af3
Rob Gifford <robjgiff@gmail.com>
2022-11-14 19:39:44 +0000

    Refactor

--- glue/build/genus/iav/iavLoadIsolateData.js
+++ glue/build/genus/iav/iavLoadIsolateData.js
@@ -2,13 +2,14 @@ var segments = [ 1, 2, 3, 4, 5, 6, 7, 8 ];
 
 _.each(segments, function(segment) {
 
-    var sourceName = 'iav-ncbi-refseqs-seg' . segment;
+    var sourceName = 'iav-ncbi-refseqs-seg' + segment;
     
 	// list the sequences in source
 	var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = '"+sourceName+"'"]);
 
 	// extract from the result a list of sequence IDs.
 	var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
+	//glue.log("INFO", "Source name", sourceName);
 
 	// for each sequence ID
 	_.each(seqIds, function(seqId) {
