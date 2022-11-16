07e9c21e6e3ec5aa70ef637f2888fb069bcb9dd4
Rob Gifford <robjgiff@gmail.com>
2022-11-16 23:17:30 +0000

    Minor edits

---
+++ glue/build/genus/iav/strainToMetadata.js
@@ -0,0 +1,14 @@
+// Fill in missing species names
+
+// NULL
+// Aves
+// Duck
+// Teal
+
+
+// list the sequences in source ncbi-refseqs
+var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'ncbi-refseqs'"]);
+// and strain is not null
+// Iterate through sequences 
+
+// 
