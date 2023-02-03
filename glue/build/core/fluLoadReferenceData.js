5360c828aef0cb5a3f747c87997bd3f971269a27
Rob Gifford <robjgiff@gmail.com>
2023-02-03 12:56:13 +0000

    Refactor (major)

--- glue/build/core/fluLoadReferenceData.js
+++ glue/build/core/fluLoadReferenceData.js
@@ -1,13 +1,35 @@
 // list the sequences in source ncbi-refseqs
-var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'ncbi-refseqs'"]);
-// extract from the result a list of sequence IDs.
-var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
-// for each sequence ID
-_.each(seqIds, function(seqId) {
-    // create an object in the custom table which uses the sequence ID as the row ID.
-    glue.command(["create", "custom-table-row", "isolate_data", seqId]);
-    // associate the corresponding sequence with this object.
-    glue.inMode("sequence/ncbi-refseqs/"+seqId, function() {
-        glue.command(["set", "link-target", "isolate_data", "custom-table-row/isolate_data/"+seqId]);
-    });
+var listSourceResult = glue.command(["list", "source"]);
+
+// extract from the result a list of sources
+var sourceNames = glue.getTableColumn(listSourceResult, "name");
+
+// for each source
+_.each(sourceNames, function(sourceName) {
+
+	//glue.log("INFO", "Source", sourceName);
+
+    var whereClause = "source.name = '"+sourceName+"'";
+    
+	var listSeqResult = glue.command(["list", "sequence", "-w", whereClause]);
+
+	// extract from the result a list of sequence IDs.
+	var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
+
+	// for each sequence ID
+	_.each(seqIds, function(seqId) {
+
+		//glue.log("INFO", "Seqeunce", seqId);
+
+		// create an object in the custom table which uses the sequence ID as the row ID.
+		glue.command(["create", "custom-table-row", "isolate", seqId]);
+	
+		// associate the corresponding sequence with this object.
+		glue.inMode("sequence/"+sourceName+"/"+seqId, function() {
+			glue.command(["set", "link-target", "isolate", "custom-table-row/isolate/"+seqId]);
+
+		});
+		
+	});
+
 });
