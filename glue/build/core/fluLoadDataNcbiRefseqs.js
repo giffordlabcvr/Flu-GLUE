062c81c14c9e16711482ef4c5d49164480897c1d
Rob Gifford <robjgiff@gmail.com>
2023-02-06 20:11:00 +0000

    Refactor

---
+++ glue/build/core/fluLoadDataNcbiRefseqs.js
@@ -0,0 +1,52 @@
+
+var segments = [ 1, 2, 3, 4, 5, 6, 7, 8 ];
+
+var species = [ 'IAV', 'IBV', 'ICV', 'IDV' ];
+
+_.each(species, function(virus) {
+
+	_.each(segments, function(segment) {
+
+		var sourceName = virus + '-ncbi-refseqs-seg' + segment;
+	
+		// list the sequences in source
+		var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = '"+sourceName+"'"]);
+
+		// extract from the result a list of sequence IDs.
+		var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
+		glue.log("INFO", "Source name", sourceName);
+	
+		// for each sequence ID
+		_.each(seqIds, function(seqId) {
+
+			// create an object in the custom table which uses the sequence ID as the row ID.
+			glue.command(["create", "custom-table-row", "isolate", seqId]);
+	
+			// associate the corresponding sequence with this object.
+			glue.inMode("sequence/"+sourceName+"/"+seqId, function() {
+		
+				glue.command(["set", "link-target", "isolate", "custom-table-row/isolate/"+seqId]);
+				glue.log("INFO", "Sequence", seqId);
+				glue.command(["set", "field", "name", species]);
+				
+				if (species == 'IAV')				
+					glue.command(["set", "field", "genus", 'Alphainfluenzavirus']);
+				}
+				if (species == 'IBV')				
+					glue.command(["set", "field", "genus", 'Betainfluenzavirus']);
+				}
+				if (species == 'ICV')				
+					glue.command(["set", "field", "genus", 'Gammainfluenzavirus']);
+				}
+				if (species == 'IDV')				
+					glue.command(["set", "field", "genus", 'Deltainfluenzavirus']);
+				}
+				
+				
+			});
+
+		});
+
+	});
+
+});
---
+++ glue/build/genus/iav/iavLoadIsolates.js
@@ -0,0 +1,93 @@
+glue.command(["multi-delete", "isolate", "-a"]);
+
+var segments = [ 1, 2, 3, 4, 5, 6, 7, 8 ];
+
+var strainObjs;
+
+glue.inMode("module/tabularUtility", function() {
+	strainObjs = glue.tableToObjects(glue.command(["load-tabular", "tabular/genus/iav/iav-strains.complete.out.tsv"]));
+});
+//glue.logInfo("strainObjs", strainObjs);
+
+var strainIDtoStrainObjs = {};
+var i = 2;
+
+_.each(strainObjs, function(strainObj) {
+
+	glue.logInfo("row", i);
+	//glue.logInfo("StrainObj", strainObj);
+	
+	i++;
+	
+	var strainPK = strainObjToStrainPK(strainObj);
+	
+ 	glue.command(["create", "custom-table-row", "isolate", strainPK]);
+
+
+	_.each(segments, function(segment) {
+
+		var key = 'segment' + segment + '_accession';
+		var segmentSeqID = strainObj[key];		
+		var sourceName = 'iav-ncbi-curated-segment-' + segment;
+		
+		
+		glue.inMode("custom-table-row/isolate/"+strainPK, function() {
+
+
+			glue.logInfo("Linking sequence", segmentSeqID);
+			glue.command(["add", "link-target", "sequence", "sequence/"+sourceName+"/"+segmentSeqID]);
+
+			var metadataFields = ["strain",
+								  "isolate", 
+								  "iso_source",
+								  "iso_country",
+								  "iso_place_name",
+								  "iso_year",
+								  "iso_month",
+								  "iso_day",
+								  "iso_host",
+								  "lab_host",
+								  "cg_subtype",
+								  "hn_subtype"];
+
+			_.each(metadataFields, function(metadataField) {
+
+				var value = handleNull(strainObj[metadataField]);
+				if (value) {
+					
+					glue.logInfo("strainObj", strainObj)
+					glue.logInfo("field", metadataField);
+					glue.logInfo("value", value);
+					glue.command(["set", "field", metadataField, value]);
+
+				}
+
+			});
+
+		});
+		
+	});
+
+});
+
+
+
+// Subroutines
+
+function strainObjToStrainPK(strainObj) {
+	
+	var strainPK = strainObj["strain_id"];
+	glue.logInfo("strainObj.strain", strainPK);
+
+	return strainPK.replace(/\//g, '_');
+	
+}
+
+function handleNull(input) {
+
+	if(input == '-') {
+		return null;
+	}
+	return input;
+}
+
