e8b2f8237b79daa6b6fd177d3c3b6c8bbaadbce5
Rob Gifford <robjgiff@gmail.com>
2023-02-07 08:31:00 +0000

    Refactor - strain level logic

--- glue/build/core/fluLoadDataNcbiRefseqs.js
+++ glue/build/core/fluLoadDataNcbiRefseqs.js
@@ -7,49 +7,57 @@ _.each(species, function(virus) {
 
 	_.each(segments, function(segment) {
 
-		var sourceName = virus + '-ncbi-refseqs-seg' + segment;
-	
-		// list the sequences in source
-		var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = '"+sourceName+"'"]);
+        if (virus == 'ICV' && segment == 8) {
+        
+        	
+        }
+        else if (virus == 'IDV' && segment == 8) {
+        
+        	// Do nothing
+        }
+        else {
 
-		// extract from the result a list of sequence IDs.
-		var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
-		glue.log("INFO", "Source name", sourceName);
-	
-		// for each sequence ID
-		_.each(seqIds, function(seqId) {
 
-			// create an object in the custom table which uses the sequence ID as the row ID.
-			glue.command(["create", "custom-table-row", "isolate", seqId]);
-	
-			// associate the corresponding sequence with this object.
-			glue.inMode("sequence/"+sourceName+"/"+seqId, function() {
-		
-				glue.command(["set", "link-target", "isolate", "custom-table-row/isolate/"+seqId]);
-				glue.log("INFO", "Sequence", seqId);
-				
-				if (virus == 'iav') {
-					glue.command(["set", "field", "name", 'IAV']);				
-					glue.command(["set", "field", "genus", 'Alphainfluenzavirus']);
-				}
-				if (virus == 'ibv') {			
-					glue.command(["set", "field", "name", 'IBV']);	
-					glue.command(["set", "field", "genus", 'Betainfluenzavirus']);
-				}
-				if (virus == 'icv') {
-					glue.command(["set", "field", "name", 'ICV']);	
-					glue.command(["set", "field", "genus", 'Gammainfluenzavirus']);
-				}
-				if (virus == 'idv') {			
-					glue.command(["set", "field", "name", 'IDV']);	
-					glue.command(["set", "field", "genus", 'Deltainfluenzavirus']);
-				}
-				
-				
+			var sourceName = virus + '-ncbi-refseqs-seg' + segment;
+
+			// list the sequences in source
+			var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = '"+sourceName+"'"]);
+
+			// extract from the result a list of sequence IDs.
+			var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
+			glue.log("INFO", "Source name", sourceName);
+
+			// for each sequence ID
+			_.each(seqIds, function(seqId) {
+
+				// associate the corresponding sequence with this object.
+				glue.inMode("sequence/"+sourceName+"/"+seqId, function() {
+			
+					if (virus == 'iav') {
+						glue.command(["set", "field", "name", 'IAV']);				
+						glue.command(["set", "field", "genus", 'Alphainfluenzavirus']);
+					}
+					if (virus == 'ibv') {			
+						glue.command(["set", "field", "name", 'IBV']);	
+						glue.command(["set", "field", "genus", 'Betainfluenzavirus']);
+					}
+					if (virus == 'icv') {
+						glue.command(["set", "field", "name", 'ICV']);	
+						glue.command(["set", "field", "genus", 'Gammainfluenzavirus']);
+					}
+					if (virus == 'idv') {			
+						glue.command(["set", "field", "name", 'IDV']);	
+						glue.command(["set", "field", "genus", 'Deltainfluenzavirus']);
+					}
+			
+			
+				});
+
 			});
-
-		});
+			
+		}
 
 	});
 
 });
+
--- glue/build/core/fluLoadIsolatesDataNcbiRefseqs.js
+++ glue/build/core/fluLoadIsolatesDataNcbiRefseqs.js
@@ -12,60 +12,100 @@ var i = 2;
 _.each(strainObjs, function(strainObj) {
 
 	glue.logInfo("row", i);
-	glue.logInfo("StrainObj", strainObj);
-	
+	//glue.logInfo("StrainObj", strainObj);
+
 	i++;
-	
+
 	var strainPK = strainObjToStrainPK(strainObj);
+	var completeGenomeSubtype = strainObj["cg_subtype"];
+	var virus = strainObj["virus_name"];
 	
- 	glue.command(["create", "custom-table-row", "isolate", strainPK]);
+	var genomeSubtypeArray = completeGenomeSubtype.split("|");
+	//glue.logInfo("genomeSubtypeArray", genomeSubtypeArray); die;
+
+	glue.command(["create", "custom-table-row", "isolate", strainPK]);
 
 
 	_.each(segments, function(segment) {
 
-		var key = 'segment' + segment + '_accession';
-		var segmentSeqID = strainObj[key];		
-		var sourceName = 'iav-ncbi-refseqs-seg' + segment;
+		// Skip segent 8 for ICV and IDV
+		if (virus == 'icv' && segment == 8) {
+	
 		
+		}
+		else if (virus == 'idv' && segment == 8) {
+	
+			// Do nothing
+		}
+		else {
+
+			var key = 'segment' + segment + '_accession';
+			var segmentSeqID = strainObj[key];
 		
-		glue.inMode("custom-table-row/isolate/"+strainPK, function() {
+			var sourceName = virus + '-ncbi-refseqs-seg' + segment;
+	
+			glue.inMode("custom-table-row/isolate/"+strainPK, function() {
 
 
-			glue.logInfo("Linking sequence", segmentSeqID);
-			glue.command(["add", "link-target", "sequence", "sequence/"+sourceName+"/"+segmentSeqID]);
+				//glue.logInfo("Linking sequence", segmentSeqID);
+				glue.command(["add", "link-target", "sequence", "sequence/"+sourceName+"/"+segmentSeqID]);
 
-			var metadataFields = ["strain",
-								  "isolate", 
-								  "iso_source",
-								  "iso_country",
-								  "iso_place_name",
-								  "iso_year",
-								  "iso_month",
-								  "iso_day",
-								  "iso_host",
-								  "lab_host",
-								  "cg_subtype",
-								  "hn_subtype"];
+				var metadataFields = [
+									  "iso_source",
+									  "iso_country",
+									  "iso_region",
+									  "iso_year",
+									  "iso_month",
+									  "iso_day",
+									  "iso_host",
+									  "lab_host",
+									  "cg_subtype",
+									  "hn_subtype"];
 
-			_.each(metadataFields, function(metadataField) {
+				_.each(metadataFields, function(metadataField) {
 
-				var value = handleNull(strainObj[metadataField]);
-				if (value) {
-					
-					glue.logInfo("strainObj", strainObj)
-					glue.logInfo("field", metadataField);
-					glue.logInfo("value", value);
-					glue.command(["set", "field", metadataField, value]);
+					var value = handleNull(strainObj[metadataField]);
+					if (value) {
+				
+						//glue.logInfo("strainObj", strainObj)
+						//glue.logInfo("field", metadataField);
+						//glue.logInfo("value", value);
+						glue.command(["set", "field", metadataField, value]);
 
-				}
+					}
+
+				});
+		
+				glue.command(["set", "field", 'complete_genome', 'TRUE']);
 
 			});
 
-		});
-		
+
+			// Set the recogniser segment field
+			glue.inMode("sequence/"+sourceName+"/"+segmentSeqID, function() {
+			
+				glue.command(["set", "field", "rec_segment", segment]);
+						
+			});
+
+			// Set the recogniser subtype field for IAV
+			if (virus == 'iav') {
+				glue.inMode("sequence/"+sourceName+"/"+segmentSeqID, function() {
+			
+					var index = segment - 1;
+					var recSubtype = genomeSubtypeArray[index];
+					glue.command(["set", "field", "rec_subtype", recSubtype]);				
+
+				});
+				
+			}
+
+		}
+	
 	});
 
 });
+
 
 
 
@@ -74,7 +114,7 @@ _.each(strainObjs, function(strainObj) {
 function strainObjToStrainPK(strainObj) {
 	
 	var strainPK = strainObj["strain_id"];
-	glue.logInfo("strainObj.strain", strainPK);
+	glue.logInfo("strain public key", strainPK);
 
 	return strainPK.replace(/\//g, '_');
 	
