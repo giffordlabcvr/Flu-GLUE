cb456b3d423be4f653055d6af4f013a5a54352e2
Rob Gifford <robjgiff@gmail.com>
2023-02-09 18:51:31 +0000

    Phylogeny build (nucs). Variation definition (IAV and IBV)

--- glue/analysis/msa/fluScanVariations.js
+++ glue/analysis/msa/fluScanVariations.js
@@ -1,50 +1,70 @@
+// project flu list variation -w "name like '%phos%'"
 
-// Scan alignments for variations
-var variations = [ "n-linked-glycosylation-o1", "n-linked-glycosylation-o2", "n-linked-glycosylation-o3", "n-linked-glycosylation-o4", "n-linked-glycosylation-o5", "n-linked-glycosylation-o6" ];
+// list features
+var codingFeatures = {};
+var resultMap = glue.command(["list", "feature","-w", "featureMetatags.name = 'CODES_AMINO_ACIDS'"]);
+var featureList = resultMap["listResult"];
+var codingFeatureList = featureList["row"];
+_.each(codingFeatureList,function(featureObj){
 
-var count = 0;
-for(var i = 0; i < variations.length; i++) {
+	//glue.log("INFO", "RESULT WAS ", featureObj);
+
+	var valueArray = featureObj["value"];
+	var codingFeatureName = valueArray[0];
+	//glue.log("INFO", "NAME WAS ", featureName)
+	codingFeatures[codingFeatureName] = featureObj;
+
+	// project flu list variation -w "name like '%phos%' and featureLoc.feature.name = 'NP' and featureLoc.referenceSequence.name like '%IAV_MASTER%'"
+	var variations;
+
+	// Scan alignments for variations
+	var count = 0;
+	for(var i = 0; i < variations.length; i++) {
 
 
-	count++;
-
-	var variationName = variations[i]
-	glue.logInfo("Processing variation: "+variationName);
-
-	var scanResult;
-	glue.inMode("alignment/AL_IFNL_MAMMAL_B", function() {
-		scanResult = glue.tableToObjects(glue.command(["variation", "member", "scan", "-r", "REF_IFNL_Mammal_b_MASTER", "-f", "orf", "-v", variationName, "-t"]));
-		//glue.log("INFO", "load result was:", scanResult);
-	});
-
-	// Iterate through results
-	_.each(scanResult, function(resultObj) {
-		
-		var sequenceID = resultObj["sequenceID"];
-		var sourceName   = resultObj["sourceName"];
-		var firstRefCodon   = resultObj["firstRefCodon"];
-
-		if (firstRefCodon) {
+		count++;
 	
-			// Update the variation field
-			glue.inMode("sequence/"+sourceName+"/"+sequenceID, function() {
+		var variationName = variations[i]
+		glue.logInfo("Processing variation: "+variationName);
+
+		// var alignmentName;
+
+
+		var scanResult;
+		glue.inMode("alignment/"+alignmentName, function() {
+			scanResult = glue.tableToObjects(glue.command(["variation", "member", "scan", "-r", "REF_IFNL_Mammal_b_MASTER", "-f", "orf", "-v", variationName, "-t"]));
+			//glue.log("INFO", "load result was:", scanResult);
+		});
+
+		// Iterate through results
+		_.each(scanResult, function(resultObj) {
+		
+			var sequenceID = resultObj["sequenceID"];
+			var sourceName   = resultObj["sourceName"];
+			var firstRefCodon   = resultObj["firstRefCodon"];
+
+			if (firstRefCodon) {
 	
- 				var variationName = 'glycosylation_o' + count;
+				// Update the variation field
+				glue.inMode("sequence/"+sourceName+"/"+sequenceID, function() {
+	
+					var variationName = 'glycosylation_o' + count;
 		
-				glue.log("INFO", "Vriation name result was:"+variationName+" sequence: "+sequenceID+" first ref codon: "+firstRefCodon);
-				glue.command(["set", "field", variationName, firstRefCodon]);	
+					glue.log("INFO", "Variation name result was:"+variationName+" sequence: "+sequenceID+" first ref codon: "+firstRefCodon);
+					
+					//glue.command(["set", "field", variationName, firstRefCodon]);	
 			
 			
-			});
+				});
 			
 		
-		}
+			}
 
 		
-	});
+		});
 
 
-}		
+	}		
 
 
-
+});
