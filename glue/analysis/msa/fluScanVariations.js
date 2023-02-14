8fc9c4edc9f965bc3c95548fbbd8e6a2151a05f5
Rob Gifford <robjgiff@gmail.com>
2023-02-14 09:35:52 +0000

    Variation scanning plus misc. other updates

--- glue/analysis/msa/fluScanVariations.js
+++ glue/analysis/msa/fluScanVariations.js
@@ -1,70 +1,119 @@
-// project flu list variation -w "name like '%phos%'"
+var viruses = [ 'IAV', 'IBV', 'ICV', 'IDV' ];
+var alignmentSets = loadAlignments();
+//glue.log("INFO", "RESULT WAS ", alignmentSets);
 
-// list features
-var codingFeatures = {};
-var resultMap = glue.command(["list", "feature","-w", "featureMetatags.name = 'CODES_AMINO_ACIDS'"]);
-var featureList = resultMap["listResult"];
-var codingFeatureList = featureList["row"];
-_.each(codingFeatureList,function(featureObj){
+// Iterate through viruses
+_.each(viruses,function(virus) {
 
-	//glue.log("INFO", "RESULT WAS ", featureObj);
+	// list variation -w "name like '%phos%' and featureLoc.referenceSequence.name like '%_IAV_%'"
+	// list variations
+	var where = "name like '%phos%' and featureLoc.referenceSequence.name like '%_" + virus + "_%'";
+	var variationResultMap = glue.command(["list", "variation","-w", where]);
+	var variationListObj = variationResultMap["listResult"];
+	var variationList = variationListObj["row"];	
+	//glue.log("INFO", "RESULT WAS ", variationList);
 
-	var valueArray = featureObj["value"];
-	var codingFeatureName = valueArray[0];
-	//glue.log("INFO", "NAME WAS ", featureName)
-	codingFeatures[codingFeatureName] = featureObj;
+	// Iterate through variations
+	_.each(variationList,function(variationObj) {
 
-	// project flu list variation -w "name like '%phos%' and featureLoc.feature.name = 'NP' and featureLoc.referenceSequence.name like '%IAV_MASTER%'"
-	var variations;
-
-	// Scan alignments for variations
-	var count = 0;
-	for(var i = 0; i < variations.length; i++) {
-
-
-		count++;
+		var valueArray = variationObj["value"];
+		var refSeqName    = valueArray[0];
+		var featureName   = valueArray[1];
+		var variationName = valueArray[2];
 	
-		var variationName = variations[i]
-		glue.logInfo("Processing variation: "+variationName);
-
-		// var alignmentName;
-
-
-		var scanResult;
-		glue.inMode("alignment/"+alignmentName, function() {
-			scanResult = glue.tableToObjects(glue.command(["variation", "member", "scan", "-r", "REF_IFNL_Mammal_b_MASTER", "-f", "orf", "-v", variationName, "-t"]));
-			//glue.log("INFO", "load result was:", scanResult);
-		});
-
-		// Iterate through results
-		_.each(scanResult, function(resultObj) {
-		
-			var sequenceID = resultObj["sequenceID"];
-			var sourceName   = resultObj["sourceName"];
-			var firstRefCodon   = resultObj["firstRefCodon"];
-
-			if (firstRefCodon) {
+		if (alignmentSets[refSeqName]) {
 	
-				// Update the variation field
-				glue.inMode("sequence/"+sourceName+"/"+sequenceID, function() {
-	
-					var variationName = 'glycosylation_o' + count;
-		
-					glue.log("INFO", "Variation name result was:"+variationName+" sequence: "+sequenceID+" first ref codon: "+firstRefCodon);
+			glue.logInfo("Processing variation: "+variationName);
+
+			refseqAlignmentList = alignmentSets[refSeqName];
+			
+			// Iterate through  alignments 
+			for(var i = 0; i < refseqAlignmentList.length; i++) {
+			
+				var alignmentName = refseqAlignmentList[i]
+				
+				glue.inMode("alignment/"+alignmentName, function() {
+				
+					scanResult = glue.tableToObjects(glue.command(["variation", "member", "scan", "-r", refSeqName, "-f", featureName, "-v", variationName]));
+					//glue.log("INFO", "load result was:", scanResult);
 					
-					//glue.command(["set", "field", variationName, firstRefCodon]);	
-			
-			
+				});
+
+				// Iterate through results
+				_.each(scanResult, function(resultObj) {
+
+					//glue.log("INFO", "load result was:", resultObj);
+
+					var sequenceID   = resultObj["sequenceID"];
+					var suffCoverage = resultObj["sourceName"];
+					var suffCoverage = resultObj["sufficientCoverage"];
+					var varIsPresent = resultObj["present"];
+					//glue.logInfo("TEST: "+varIsPresent+" in "+sequenceID);
+					
+					if (varIsPresent) {
+					
+						// Update the variation field
+						glue.logInfo("Variation: "+variationName+" is present in "+sequenceID);
+	
+					}
+					else {
+						
+						glue.logInfo("Variation: "+variationName+" is NOT present in "+sequenceID);
+					
+					
+					}
+	
 				});
 			
-		
 			}
-
+			
+		}
+		else {
+			
+			glue.logInfo("Skipping variation"+variationName+" - it's reference "+refSeqName+" does not constrain any alignments");
 		
-		});
-
-
-	}		
-
+		}
+	
+	});
 
 });
+
+
+// Subroutines
+function loadAlignments() {
+
+	var alignmentSets = {};
+	
+	
+	// list alignments
+	var where = "name not like '%_ROOT_%'";
+	var alignmentResultMap = glue.command(["list", "alignment","-w", where]);
+	var alignmentListObj = alignmentResultMap["listResult"];
+	var alignmentList = alignmentListObj["row"];
+	//glue.log("INFO", "RESULT WAS ", alignmentList);
+
+	_.each(alignmentList,function(alignmentObj){
+
+		var valueArray    = alignmentObj["value"];
+		var alignmentName = valueArray[0];
+		var refSeqName    = valueArray[2];	
+		glue.log("INFO", "RESULT WAS ", refSeqName);
+
+		if (alignmentSets[refSeqName]) {
+		
+			refseqAlignmentList = alignmentSets[refSeqName];
+			glue.log("INFO", "RESULT WAS ", refseqAlignmentList);
+			refseqAlignmentList.push(alignmentName);
+			
+		}
+		else {
+			var refseqAlignmentList = [];
+			refseqAlignmentList.push(alignmentName);					
+			alignmentSets[refSeqName] = refseqAlignmentList;		
+		}
+		
+	});
+
+	return alignmentSets;
+
+}
